fbdb8fb959ac848ed7acdcde13da152bafa814a8
Seshpenguin <seshan10@me.com>
2017-11-03 21:56:46 -0400

    First (real) commit! Woooo

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# iona
+![Iona Logo](https://i.imgur.com/ddhX6vp.png)
+# Welcome to iona
 Smart Home Server GNU+Linux
+## What is it?!
+This version of iona (known internally as "Feature-Complete Iona") is a GNU+Linux distro that will combine all the core features of the (eventually-to-be-released) iona-next (a combined OS+Hardware product with a custom gui yada yada)...
+
+Basically this repo consists of the various iona modules that make up iona GNU+Linux. Iona will have the follow features:
+- [x] Traditional DE (ex. KDE, Budgie, etc) w/ standard desktop apps (FireFox, etc)
+- [x] Kodi
+- [ ] Remotely accessible web UI
+- [ ] Smart home automation (Smart Lights, Thermostats, all those things)
+- [ ] Storage Server w/ SAMBA, Web UI, etc (with support for on-the-fly storage expansion)
+- [ ] Voice Assistant (Alexa or something)
+- [ ] AND MORE!!!
+
+So yea... let's see how far this gets! :tada:
+
+## Timeline
+Not even close (╯°□°）╯︵ ┻━┻
---
+++ screensaver/server.js
@@ -0,0 +1,122 @@
+/*
+ * Iona Screensaver Daemon
+ * 
+ */
+
+console.log("Starting iona screensaver daemon...")
+
+const ioHook = require('iohook');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('test.sql');
+const { exec } = require('child_process');
+
+var islocked = false;
+var lockCommand = "dm-tool lock";
+var lockTime = 0;
+
+
+db.serialize(function () {
+    /*
+    var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+    for (var i = 0; i < 10; i++) {
+        stmt.run("Ipsum " + i);
+    }
+    stmt.finalize();
+    */
+    //grab the things we need from the database.
+    db.each("SELECT * FROM config", function (err, row) {
+        console.log(row);
+        lockTime = parseInt(row.locktime);
+    });
+});
+
+
+// create a timer object
+function Timer(fn, t) {
+    var timerObj = setInterval(fn, t);
+
+    this.stop = function () {
+        if (timerObj) {
+            clearInterval(timerObj);
+            timerObj = null;
+        }
+        return this;
+    }
+
+    // start timer using current settings (if it's not already running)
+    this.start = function () {
+        if (!timerObj) {
+            this.stop();
+            timerObj = setInterval(fn, t);
+        }
+        return this;
+    }
+
+    // start with new interval, stop current interval
+    this.reset = function (newT) {
+        t = newT;
+        return this.stop().start();
+    }
+}
+
+var timer = new Timer(function () {
+    console.log("Lock");
+    lock();
+}, 2000);
+
+// check if the keyboard or mouse has been used.
+ioHook.on("keypress", event => {
+    console.log(event);
+    // {keychar: 'f', keycode: 19, rawcode: 15, type: 'keypress'}
+    dontLock();
+});
+ioHook.on("mousemove", event => {
+    //console.log(event);
+    // {keychar: 'f', keycode: 19, rawcode: 15, type: 'keypress'}
+    dontLock();
+});
+ioHook.on("mouseclick", event => {
+    console.log(event);
+    // {keychar: 'f', keycode: 19, rawcode: 15, type: 'keypress'}
+    dontLock();
+});
+ioHook.start();
+
+
+function dontLock() {
+    //reset the lock timer
+    //console.log("Reseting lock timer."); wayyyy too spammy :P
+    timer.reset(lockTime);
+}
+
+function lock() {
+    if (!islocked) {
+        console.log("Locking screen...");
+        exec('echo this is would be the command', (err, stdout, stderr) => {
+            if (err) {
+                // node couldn't execute the command
+                return;
+            }
+
+            // the *entire* stdout and stderr (buffered)
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+        });
+        islocked = true;
+    } else {
+        console.log("Already in lock screen.")
+    }
+
+}
+// switch interval to 10 seconds
+//timer.reset(10000);
+
+// stop the timer
+//timer.stop();
+
+// start the timer
+//timer.start();
+
+
+
+db.close();
