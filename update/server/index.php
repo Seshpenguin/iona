ceae787dde63498013fa062f2a24c6df7b7cd465
Seshpenguin <seshan10@me.com>
2017-11-07 10:57:20 -0500

    Added Update Services

---
+++ update/server/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Iona Updates</title>
+  <style>
+    header,
+    main,
+    footer {
+      padding-left: 300px;
+    }
+    .nav-wrapper {
+      padding-left: 330px;
+    }
+    .card {
+      height: 85vh;
+    }
+    .align-with-sidenav {
+      padding-left: 330px;
+    }
+    @media only screen and (max-width: 992px) {
+      header,
+      main,
+      footer {
+        padding-left: 0;
+      }
+    }
+  </style>
+</head>
+
+<body>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo">Iona Update Services</a>
+      <ul class="right hide-on-med-and-down">
+      <li class="active"><a href="collapsible.html">Home</a></li>
+      <li><a href="collapsible.html">Settings</a></li>
+      <li><a href="collapsible.html">Help</a></li>
+      </ul>
+    </div>
+  </nav>
+  <ul id="slide-out" class="side-nav fixed">
+    <li><img src="images/ionacolours.png" width="100%" height="100%"></img></li>
+    <li><a class="waves-effect waves-light btn-large">Check for Updates  <i class="material-icons left">cloud</i></a></li>
+    
+    <a class="twitter-timeline" href="https://twitter.com/Seshpenguin?ref_src=twsrc%5Etfw">News on Updates</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> 
+            
+  </ul>
+  <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
+
+          <div class="card grey darken-1 align-with-sidenav">
+            <div class="card-content white-text">
+              <span class="card-title">Card Title</span>
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+              <div class="progress">
+                <div class="indeterminate"></div>
+              </div>
+            </div>
+            <div class="card-action align-with-sidenav">
+              <a href="#">This is a link</a>
+              <a href="#">This is a link</a>
+            </div>
+          </div>
+
+            
+  <!--Import jQuery before materialize.js-->
+  <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  <script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+
+</html>
